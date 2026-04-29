0ce57b1792e1c959811afa0bbec9738958fda1b3
Suraj Patidar <167690910+surajpatidar1@users.noreply.github.com>
2026-04-29 16:39:34 +0530

    hr

--- src/components/employee/sidebar.tsx
+++ src/components/employee/sidebar.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use client"
 
 import React from "react"
@@ -79,4 +80,68 @@ export default function Sidebar() {
       </div>
     </div>
   )
+=======
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+import { Home, ClipboardList, Clock, User, ChevronLeft, ChevronRight } from "lucide-react"
+import { useRouter, usePathname } from "next/navigation";
+
+  
+const navItems = [
+   {  href: "/dashboard/employee", icon: Home },
+    { href: "/dashboard/employee/tasks", icon: ClipboardList },
+    { href: "/dashboard/employee/attendance", icon: Clock },
+    {  href: "/dashboard/employee/profile", icon: User }
+];
+
+export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <div className="h-screen flex bg-[#ACC8A2]/70  items-center px-4">
+      <div className="w-20 bg-white/70 backdrop-blur-xl shadow-xl rounded-3xl flex flex-col items-center gap-6 border border-gray-200">
+
+        <div className="flex-1" />
+
+        <div className="flex flex-col items-center gap-6">
+          {navItems.map((item, index) => {
+            const Icon = item.icon;
+            const isActive = pathname === item.href;
+
+            return (
+              <button
+                key={index}
+                onClick={() => router.push(item.href)}
+                className="group relative w-12 h-12 flex items-center justify-center rounded-2xl overflow-hidden"
+              >
+                {/* Ripple */}
+                <span className="absolute inset-0 rounded-2xl bg-blue-500/10 scale-0 group-hover:scale-150 transition-transform duration-500"></span>
+
+                {/* Liquid glow */}
+                <span className="absolute w-10 h-10 bg-white/90 rounded-full blur-md opacity-0 group-hover:opacity-100 group-hover:animate-pulse"></span>
+
+                {/* Icon */}
+                <div
+                  className={`relative z-10 flex items-center justify-center w-full h-full rounded-2xl transition-all duration-300
+                  ${
+                    isActive
+                      ? "bg-[#465e3e] text-white shadow-md"
+                      : "bg-[#ACC8A2]/30 group-hover:bg-[#1A2517]"
+                  }`}
+                >
+                  <Icon className={`w-5 h-5 ${isActive ? "text-white" : "text-gray-900"} group-hover:text-white`} />
+                </div>
+              </button>
+            );
+          })}
+        </div>
+
+        <div className="flex-1" />
+      </div>
+    </div>
+  );
+>>>>>>> 0958281 (hr)
 }
